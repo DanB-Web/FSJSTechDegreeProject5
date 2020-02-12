fea7030e7b9ad29817c401bc67e9782328dfe5dd
DanB-Web <dan.bayford@gmail.com>
2020-02-12 13:39:20 +0000

    Linked Data to Modal Windows
    Data displaying on modal window now after click event

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const userAPI = "https://randomuser.me/api/?results=12&inc=picture,name,email,location,cell,dob&nat=au,ca,gb,nz,us";
 
+const body = document.getElementsByTagName("body");
 const header = document.querySelector(".header-inner-container");
 const mainHeader = document.getElementsByTagName("header");
 const jsScript = document.getElementsByTagName("script");
 
+const users = document.getElementsByClassName("card");
 
 /*API Fetch Request - the returned request is passed to checkStatus to confirm the Promise status. If successful, the returned JSON is parsed
 in a JS object. If not successful, an error is thrown back and handled by the catch method*/
@@ -32,10 +34,10 @@ function checkStatus(response) {
 /*Function to fetch API data and then pass it onto the userCards function*/
 
 fetchData(userAPI) 
-    //.then (data => console.log(data))
     .then (data => userCards(data))
+    .then (data => addListener(data))
 
-/*Function to generate user cards*/
+/*Function to generate user cards - could refactor and place const inside template literals*/
 
 function userCards (data) {
 
@@ -43,35 +45,35 @@ function userCards (data) {
 
         const user = document.createElement("div");
 
-        const image = data.results[i].picture.large;
-        const firstName = data.results[i].name.first;
-        const lastName = data.results[i].name.last;
-        const email = data.results[i].email;
-        const city = data.results[i].location.city;
-        const state = data.results[i].location.state;
-
-        
+        //const image = data.results[i].picture.large;
+        //const firstName = data.results[i].name.first;
+        //const lastName = data.results[i].name.last;
+        //const email = data.results[i].email;
+        //const city = data.results[i].location.city;
+        //const state = data.results[i].location.state;
 
         user.classList.add("card")
-
+    
         user.innerHTML = `<div class="card-img-container">
-                          <img class="card-img" src="${image}" alt="profile picture">
+                          <img class="card-img" src="${data.results[i].picture.large}" alt="profile picture">
                           </div>
                           <div class="card-info-container">
-                          <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-                          <p class="card-text">${email}</p>
-                          <p class="card-text cap">${city}, ${state}</p>
+                          <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                          <p class="card-text">${data.results[i].email}</p>
+                          <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
                           </div>`;
 
         gallery.appendChild(user);
     }
+
+    return data; 
 } 
 
 /*Function to iterate over users with search term*/
 
 function cardSearch (searchString) {
 
-    const users = document.getElementsByClassName("card");
+    //const users = document.getElementsByClassName("card");
     const names = document.getElementsByClassName("card-name cap");
     const userArray = Array.from(users);
     const namesArray = Array.from(names);
@@ -93,7 +95,7 @@ function cardSearch (searchString) {
 
 function resetPage () {
 
-    const users = document.getElementsByClassName("card");
+    //const users = document.getElementsByClassName("card");
     const userArray = Array.from(users);
 
     userArray.forEach(user => user.style.display = "")
@@ -124,10 +126,46 @@ gallery.classList.add("gallery")
 gallery.setAttribute("id", "gallery");
 mainHeader[0].parentNode.insertBefore(gallery, jsScript[0]);
 
+/*HTML Modal Pop Up*/
+
+function createModal (i, data) {
+
+console.log(data);
+
+const modal = document.createElement("div")
+modal.classList.add("modal-container");
+
+const dob = data.results[i].dob;
+const bday = dob.date.slice(0,10);
+
+
+modal.innerHTML = `<div class="modal">
+                   <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                   <div class="modal-info-container">
+                   <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+                   <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                   <p class="modal-text">${data.results[i].email}</p>
+                   <p class="modal-text cap">${data.results[i].location.city}</p>
+                   <hr>
+                   <p class="modal-text">${data.results[i].cell}</p>
+                   <p class="modal-text">${data.results[i].location.street.number} ${data.results[i].location.street.name}, ${data.results[i].location.city}, ${data.results[i].location.postcode}</p>
+                   <p class="modal-text">Birthday: ${bday}</p>
+                   </div>
+                   </div>
+
+                    // IMPORTANT: Below is only for exceeds tasks 
+                    <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                    </div>`;
+
+body[0].appendChild(modal);
+
+}
+
 /*EVENT LISTENERS*/
 
 const searchButton = document.getElementById("search-submit");
-
 
 searchButton.addEventListener("click", event => {
 
@@ -141,3 +179,17 @@ searchButton.addEventListener("click", event => {
     {resetPage();}
 
 });
+
+
+function addListener (data) {
+
+    const userDivs = document.getElementsByClassName("card");    
+    
+    for (let i = 0; i < userDivs.length; i++) {
+        
+        userDivs[i].addEventListener ("click", event => {
+
+        createModal(i, data);})
+    }
+}
+
